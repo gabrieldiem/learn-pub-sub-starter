bd553ae7f206e9c19c92bc1c5d44fc0d9914b647
gabrieldiem <gabriel.diem5@gmail.com>
2025-09-22 11:11:13 -0300

    added repl to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -12,17 +12,78 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func gameloop(conn *amqp.Connection) {
-	username, err := gamelogic.ClientWelcome()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func connectExchange(conn *amqp.Connection, username string) (*amqp.Channel, amqp.Queue, error) {
 	exchange := routing.ExchangePerilDirect
 	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
 	routingKey := routing.PauseKey
 	var queueType internal.SimpleQueueType = internal.Transient
-	internal.DeclareAndBind(conn, exchange, queueName, routingKey, queueType)
+	return internal.DeclareAndBind(conn, exchange, queueName, routingKey, queueType)
+}
+
+func processInput(rabbitChan *amqp.Channel, input []string, gameState *gamelogic.GameState) bool {
+	action := input[0]
+
+	switch action {
+	case "spawn":
+		err := gameState.CommandSpawn(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	case "move":
+		_, err := gameState.CommandMove(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	case "status":
+		gameState.CommandStatus()
+
+	case "help":
+		gamelogic.PrintClientHelp()
+
+	case "spam":
+		log.Println("Spamming not allowed yet!")
+
+	case "quit":
+		gamelogic.PrintQuit()
+		return true
+
+	default:
+		log.Println("Did not understand")
+	}
+
+	return false
+}
+
+func gameloop(conn *amqp.Connection) bool {
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal(err)
+		return true
+	}
+
+	rabbitChan, _, err := connectExchange(conn, username)
+	if err != nil {
+		log.Fatal(err)
+		return true
+	}
+
+	gameState := gamelogic.NewGameState(username)
+
+	var input []string
+	for len(input) == 0 {
+		input = gamelogic.GetInput()
+
+		if len(input) != 0 {
+			shouldExit := processInput(rabbitChan, input, gameState)
+			if shouldExit {
+				return true
+			}
+			input = []string{}
+		}
+	}
+	return false
 }
 
 func main() {
@@ -38,7 +99,13 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 
-	gameloop(conn)
+	shouldExit := gameloop(conn)
+	log.Println("Finished loop")
+
+	if shouldExit {
+		log.Println("Exiting via loop exit")
+		return
+	}
 
 	<-sigChan
 	log.Println("Shutting down...")
